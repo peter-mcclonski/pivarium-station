91ae5ac74a8fb2029e2ca9db6d2850500cbb983e
= <pajablons@gmail.com>
2021-11-25 03:54:29 -0500

    Corrected DHT22 implementation

--- Sensor.py
+++ Sensor.py
@@ -3,11 +3,11 @@ from typing import Literal
 
 class Sensor:
     uuid: str
-    stype: Literal["humidity", "temperature"]
+    stype: list
     connType: Literal["dht22"]
     hwAddress: str
 
-    def __init__(self, uuid: str, stype: Literal["humidity", "temperature"], connType: Literal["dht22"], hwAddress: str):
+    def __init__(self, uuid: str, stype: list, connType: Literal["dht22"], hwAddress: str):
         self.uuid = uuid
         self.stype = stype
         self.connType = connType
--- sensorimpl/DHT22.py
+++ sensorimpl/DHT22.py
@@ -3,24 +3,20 @@ import board
 
 from Sensor import Sensor
 
-boardLookup = {
-    '0': board.GPIO0,
-    '1': board.GPIO1,
-    '2': board.GPIO2,
-    '3': board.GPIO3,
-    '4': board.GPIO4,
-}
-
 
 class DHT22(Sensor):
     dht_device: adafruit_dht.DHT22
 
     def __init__(self, uuid, stype, connType, hwAddress):
-        super.__init__(self, uuid, stype, connType, hwAddress)
-        self.dht_device = adafruit_dht.DHT22(boardLookup[self.hwAddress])
+        Sensor.__init__(self, uuid, stype, connType, hwAddress)
+        self.dht_device = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 
     def getStatus(self):
-        if self.stype == 'humidity':
-            return self.dht_device.humidity
-        else:
-            return self.dht_device.temperature
+        try:
+            return {
+                'humidity': self.dht_device.humidity,
+                'temperature': self.dht_device.temperature
+            }
+        except Exception as e:
+            return {}
+
